897f5ff97bd87fb3d8b2dcc7c9edfed0d3205e53
cris <path2017@126.com>
2018-10-09 10:37:05 +0800

    Updates

--- precache-manifest.04eb68b5ef9295e3078c2fd092f2c144.js
+++ precache-manifest.04eb68b5ef9295e3078c2fd092f2c144.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/webfront/static/js/runtime~main.816b3e69.js"
   },
   {
-    "revision": "a715001b5d06bbdc0b3e",
-    "url": "/webfront/static/js/main.a715001b.chunk.js"
+    "revision": "deb9498729e1a1e77d04",
+    "url": "/webfront/static/js/main.deb94987.chunk.js"
   },
   {
     "revision": "6105a37ce77693a684b2",
     "url": "/webfront/static/js/1.6105a37c.chunk.js"
   },
   {
-    "revision": "a715001b5d06bbdc0b3e",
+    "revision": "deb9498729e1a1e77d04",
     "url": "/webfront/static/css/main.b8daccff.chunk.css"
   },
   {
-    "revision": "9a8590bc2eef6c53d7ab0b1886eb9f63",
+    "revision": "c53d6743814a17834e3092b679b430f1",
     "url": "/webfront/index.html"
   }
 ];
