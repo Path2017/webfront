163cbe5479088883cea33088e00e46dd7c24dc11
cris <path2017@126.com>
2018-10-09 10:21:01 +0800

    add 部署

---
+++ build/precache-manifest.3295d87fae843259024a93238b2bb58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webfront/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "816b3e69d2f6e6eecbd7",
+    "url": "/webfront/static/js/runtime~main.816b3e69.js"
+  },
+  {
+    "revision": "a715001b5d06bbdc0b3e",
+    "url": "/webfront/static/js/main.a715001b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/webfront/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a715001b5d06bbdc0b3e",
+    "url": "/webfront/static/css/main.b8daccff.chunk.css"
+  },
+  {
+    "revision": "9a8590bc2eef6c53d7ab0b1886eb9f63",
+    "url": "/webfront/index.html"
+  }
+];
